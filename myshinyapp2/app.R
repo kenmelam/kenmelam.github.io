78959c9c456c82e751e60b693d931b3cfdf3682c
kenmelam <163423839+kenmelam@users.noreply.github.com>
2024-04-09 21:17:43 +0800

    apr9

--- myshinyapp2/app.R
+++ myshinyapp2/app.R
@@ -1,7 +1,6 @@
-ui <- navbarPage(title = "Player",
+ui <- navbarPage(title = "Taylor Swift's Top Hits on Spotify",
                  tabPanel(id = "tab",
-                          title = strong("Taylor Swift"),
-                          icon = icon("circle-info"),
+                          title = strong("Can you spot any patterns in her top hits?"),
                           sidebarLayout(
                             sidebarPanel = '',
                             mainPanel(
@@ -9,7 +8,7 @@ ui <- navbarPage(title = "Player",
                                 style="border-radius:12px",
                                 src="https://open.spotify.com/embed/artist/06HL4z0CvFAxyc27GXpf02?utm_source=generator",
                                 width="100%",
-                                height="380",
+                                height="500",
                                 frameBorder="0",
                                 allowfullscreen="",
                                 allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture",
