dd4681a8a246839d1a011a4215d7a644a4032faa
kenmelam <163423839+kenmelam@users.noreply.github.com>
2024-04-09 19:32:33 +0800

    apr9

--- myshinyapp/app.R
+++ myshinyapp/app.R
@@ -3,7 +3,20 @@ library(tidyverse)
 # Sample 
 taylorswift <- read.csv("taylorswift.csv") 
 
-# 1: plot the different genres present in her albums  
+library(stringr)
+library(ggplot2)
+genres <- c('Country', 'English', 'USA', 'Country Rock', 'American Folk', 'Folk', 'Ballad')
+cur_col <- taylorswift$song_tags
+taylorswift$Country = as.numeric(str_detect(cur_col, pattern = genres[[1]]))
+taylorswift$English = as.numeric(str_detect(cur_col, pattern = genres[[2]]))
+taylorswift$USA = as.numeric(str_detect(cur_col, pattern = genres[[3]]))
+taylorswift$Country_Rock = as.numeric(str_detect(cur_col, pattern = genres[[4]]))
+taylorswift$American_Folk = as.numeric(str_detect(cur_col, pattern = genres[[5]]))
+taylorswift$Folk = as.numeric(str_detect(cur_col, pattern = genres[[6]]))
+taylorswift$Ballad = as.numeric(str_detect(cur_col, pattern = genres[[7]]))
+
+
+# 1: plot bar chart for different genres present in her albums  
 # Define UI for application
 ui <- fluidPage(  titlePanel("An Analysis of Taylor Swift's Discography"),
                   sidebarLayout(    sidebarPanel(
@@ -16,16 +29,20 @@ ui <- fluidPage(  titlePanel("An Analysis of Taylor Swift's Discography"),
 server <- function(input, output) {
   output$genreplot <- renderPlot({
     df <- taylorswift %>%
-      filter(album_title == input$album_title) %>%  
-      group_by(genre_vectors) %>%
-      summarise(n = n()) 
+      filter(album_title == input$album_title)
     
     if (nrow(df) > 0) {
-      ggplot(df, aes(x = genre_vectors, y = n, fill = genre_vectors)) + 
+      df %>%
+        select(Country:Ballad) %>%
+        pivot_longer(Country:Ballad, names_to = "genre", values_to = "val") %>%
+        group_by(genre) %>%
+        summarise(n = mean(val, na.rm = T)) %>%
+      ggplot(aes(x = genre, y = n, fill = genre)) + 
         geom_bar(stat = "identity") + 
         theme_minimal() + 
         labs(title = "Genres Covered in Selected Album", x = "Genres", y = "Count") +
-        scale_fill_discrete(name = "Genre")
+        scale_fill_discrete(name = "Genre") +
+        scale_fill_viridis_d()
     }
   })
 }
